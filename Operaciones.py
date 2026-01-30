c0f08d760dbf8f05283968233a56918ad5c153f2
Valeria <valeria.loslipo@gmail.com>
2026-01-30 12:37:50 +0100

    valeria

--- Operaciones.py
+++ Operaciones.py
@@ -1,11 +1,3 @@
-<<<<<<< HEAD
-print("Introduce un numero:")
-num1 = int(input())
-print("Introduce otro numero:")
-num2 = int(input())
-suma = num1 + num2
-print("La suma es:", suma)
-=======
 #Calcula el valor exponencial de un número dado una base y un exponente.
 # Solicita al usuario que ingrese la base y el exponente.
 base = float(input("Ingrese la base: "))
@@ -17,3 +9,11 @@ resultado = base ** exponente
 # Muestra el resultado.
 print("Resultado:", resultado)
 >>>>>>> 1d46f66d5782b7688b4b6030ad63caaf5c3daf74
+
+
+print("Introduce un numero:")
+num1 = int(input())
+print("Introduce otro numero:")
+num2 = int(input())
+suma = num1 + num2
+print("La suma es:", suma)
