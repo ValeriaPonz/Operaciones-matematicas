1d46f66d5782b7688b4b6030ad63caaf5c3daf74
Carol.mp92 <c.medrano92@hotmail.com>
2026-01-30 12:03:50 +0100

    A

--- Operaciones.py
+++ Operaciones.py
@@ -1,1 +1,10 @@
+#Calcula el valor exponencial de un número dado una base y un exponente.
+# Solicita al usuario que ingrese la base y el exponente.
+base = float(input("Ingrese la base: "))
+exponente = float(input("Ingrese el exponente: "))
 
+# Calcula el valor exponencial utilizando el operador de potencia (**).
+resultado = base ** exponente
+
+# Muestra el resultado.
+print("Resultado:", resultado)
